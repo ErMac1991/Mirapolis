f7065849333f473962ad823c127bf8d467e07863
Баскаков Евгений <ermac1361@gmail.com>
2025-10-30 03:14:24 +0400

    Заполнение файла Персонажа

--- src/main/java/CreateCharacter.java
+++ src/main/java/CreateCharacter.java
@@ -16,47 +16,47 @@ public class CreateCharacter {
 
     }
 
-    public void createCharObject(){
+    public void createCharObject() throws IOException {
+
         Char newChar = new Char();
+
+        Files.writeString(Path.of("F:\\Проекты\\Стримы\\Mirapolis\\Персонажи\\" + newChar.login + "\\" + newChar.login + ".txt"),
+                "{\"userData\":{" +
+                        "\"nickname\":\""+ newChar.login +"\"," +
+                        "\"level\":1," +
+                        "\"body\"{:" +
+                            "\"lefthand\":\"Плоть\"," +
+                            "\"righthand\":\"Плоть\"," +
+                            "\"leftleg\":\"Плоть\"," +
+                            "\"rightleg\": \"Плоть\"," +
+                            "\"body\": \"Плоть\", " +
+                            "\"head\": \"Плоть\"}," +
+                        "\"stats\":{" +
+                            "\"endurance\":5," +
+                            "\"attentiveness\":5," +
+                            "\"reaction\":5," +
+                            "\"strength\":5," +
+                            "\"inventiveness\":5," +
+                            "\"luck\":5," +
+                            "\"fame\":0," +
+                            "\"mentalhealth\":95}," +
+                        "\"mods\":{" +
+                            "\"endurance\":0," +
+                            "\"attentiveness\":0," +
+                            "\"reaction\":0," +
+                            "\"strength\":0," +
+                            "\"inventiveness\":0," +
+                            "\"luck\":0," +
+                            "\"fame\":0," +
+                            "\"mentalhealth\":0}}," +
+                "\"inventory\":{" +
+                        "\"bag\":" +
+                            "{\"place1\":\"Пусто\"," +
+                            "\"place2\":\"Пусто\"," +
+                            "\"place3\":\"Пусто\"," +
+                            "\"place4\":\"Пусто\"}," +
+                        "\"storage\":[]}," +
+                        "\"quest\":null}");
     }
 }
 
-/*
-Files.writeString(Path.of("F:\\Проекты\\Стримы\\Mirapolis\\Персонажи\\" + userLogin + "\\" + userLogin + ".txt"),
-        "{\"userData\":{" +
-        "\"nickname\":\""+ userLogin +"\"," +
-        "\"level\":1," +
-        "\"body\"{:" +
-        "\"lefthand\":\"Плоть\"," +
-        "\"righthand\":\"Плоть\"," +
-        "\"leftleg\":\"Плоть\"," +
-        "\"rightleg\": \"Плоть\"," +
-        "\"body\": \"Плоть\"}," +
-        "\"stats\":{" +
-        "\"endurance\":5," +
-        "\"attentiveness\":5," +
-        "\"reaction\":5," +
-        "\"strength\":5," +
-        "\"inventiveness\":5," +
-        "\"luck\":5," +
-        "\"fame\":0," +
-        "\"mentalhealth\":95}," +
-        "\"mods\":{" +
-        "\"endurance\":0," +
-        "\"attentiveness\":0," +
-        "\"reaction\":0," +
-        "\"strength\":0," +
-        "\"inventiveness\":0," +
-        "\"luck\":0," +
-        "\"fame\":0," +
-        "\"mentalhealth\":0}}," +
-        "\"inventory\":{" +
-        "\"bag\":" +
-        "{\"place1\":\"Пусто\"," +
-        "\"place2\":\"Пусто\"," +
-        "\"place3\":\"Пусто\"," +
-        "\"place4\":\"Пусто\"}," +
-        "\"storage\":[]}," +
-        "\"quest\":null}");
-
- */
