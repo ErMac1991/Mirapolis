27a40673d634d8c368a4c79f7517d923c9d19682
Баскаков Евгений <ermac1361@gmail.com>
2025-12-04 04:55:30 +0400

    Созданы Enum класс и класс для генерации противников-людей

--- src/main/java/EnemyHuman.java
+++
@@ -1,3 +0,0 @@
-public class EnemyHuman extends Unit{
-
-}
---
+++ src/main/java/EnemyHumanCreator.java
@@ -0,0 +1,109 @@
+import java.io.File;
+import java.io.IOException;
+
+public class EnemyHumanCreator extends Unit{
+    int enemyHumanID;
+    final File EnemyIDCounterFile = new File("F:\\Проекты\\Стримы\\Mirapolis\\Системные файлы\\EnemiesCounter.txt");
+
+    // ГЕТТЕРЫ И СЕТТЕРЫ
+
+    public int getEnemyHumanID() {
+        return enemyHumanID;
+    }
+    public void setEnemyHumanID(int enemyHumanID) {
+        this.enemyHumanID = enemyHumanID;
+    }
+    public int getLevel() {
+        return level;
+    }
+    public void setLevel(int level) {
+        this.level = level;
+    }
+    public String getSubject() {
+        return subject;
+    }
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+    public int getEndurance() {
+        return endurance;
+    }
+    public void setEndurance(int endurance) {
+        this.endurance = endurance;
+    }
+    public int getAttentiveness() {
+        return attentiveness;
+    }
+    public void setAttentiveness(int attentiveness) {
+        this.attentiveness = attentiveness;
+    }
+    public int getReaction() {
+        return reaction;
+    }
+    public void setReaction(int reaction) {
+        this.reaction = reaction;
+    }
+    public int getStrength() {
+        return strength;
+    }
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+    public int getEnduranceMod() {
+        return enduranceMod;
+    }
+    public void setEnduranceMod(int enduranceMod) {
+        this.enduranceMod = enduranceMod;
+    }
+    public int getAttentivenessMod() {
+        return attentivenessMod;
+    }
+    public void setAttentivenessMod(int attentivenessMod) {
+        this.attentivenessMod = attentivenessMod;
+    }
+    public int getReactionMod() {
+        return reactionMod;
+    }
+    public void setReactionMod(int reactionMod) {
+        this.reactionMod = reactionMod;
+    }
+    public int getStrengthMod() {
+        return strengthMod;
+    }
+    public void setStrengthMod(int strengthMod) {
+        this.strengthMod = strengthMod;
+    }
+    public String getFirstBagPlace() {
+        return firstBagPlace;
+    }
+    public void setFirstBagPlace(String firstBagPlace) {
+        this.firstBagPlace = firstBagPlace;
+    }
+    public String getSecondBagPlace() {
+        return secondBagPlace;
+    }
+    public void setSecondBagPlace(String secondBagPlace) {
+        this.secondBagPlace = secondBagPlace;
+    }
+    public String getThirdBagPlace() {
+        return thirdBagPlace;
+    }
+    public void setThirdBagPlace(String thirdBagPlace) {
+        this.thirdBagPlace = thirdBagPlace;
+    }
+    public String getFourthBagPlace() {
+        return fourthBagPlace;
+    }
+    public void setFourthBagPlace(String fourthBagPlace) {
+        this.fourthBagPlace = fourthBagPlace;
+    }
+    public File getEnemyIDCounterFile() {
+        return EnemyIDCounterFile;
+    }
+
+    public void createNewEnemyHuman(QuestConstructor quest, EnemyHumanCreator enemyHuman) throws IOException {
+        enemyHuman.setEnemyHumanID(FileManager.getID(enemyHuman.getEnemyIDCounterFile()));
+
+    }
+
+}
